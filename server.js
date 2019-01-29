a552a3063634261a19c3778167ceed075eda87a6
Negar Amiri <negamiri@gmail.com>
2019-01-29 15:01:31 +0000

    Set up skeleton

---
+++ server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+require('dotenv').config();
+
+const PORT        = process.env.PORT || 8080;
+const ENV         = process.env.ENV || "development";
+const express     = require("express");
+const bodyParser  = require("body-parser");
+const sass        = require("node-sass-middleware");
+const app         = express();
+
+const knexConfig  = require("./knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+const morgan      = require('morgan');
+const knexLogger  = require('knex-logger');
+
+app.use(morgan('dev'));
+
+app.use(knexLogger(knex));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
